03ecca48e3c36dbe73f8ed7371dcc06378f1d537
Erwin Feng <efeng732@gmail.com>
2020-11-08 07:54:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
 def reverse_each_word(string)
   new_array = array.split(" ")
-  backwards_array = new_array.each {|word| word.reverse!}
+  backwards_array = new_array.each {|word| word.reverse}
+  
   
   
   
